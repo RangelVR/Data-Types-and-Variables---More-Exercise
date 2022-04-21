ca89a052f899c0888317e807635200439c449b33
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-21 23:35:06 +0300

    Add files via upload

---
+++ 01. Data Type Finder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                
+                
+
+                
+
+               
+            }
+        }
+    }
+}
