8ffc022f73455121e20573da5b88bb0ea36dfa49
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-21 23:35:50 +0300

    Update Program.cs

--- 01. Data Type Finder/Program.cs
+++ 01. Data Type Finder/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _01._Data_Type_Finder
 {
@@ -9,20 +9,79 @@ namespace _01._Data_Type_Finder
 
             while (true)
             {
-                string command = Console.ReadLine();
-
-                if (command == "END")
+                var input = Console.ReadLine();
+                string dataType = "";
+                if (input == "END")
                 {
                     break;
                 }
-
-                
-                
-
-                
-
-               
+                try
+                {
+                    int number = int.Parse(input);
+                    dataType = "integer";
+                }
+                catch (FormatException)
+                {
+                    try
+                    {
+                        double number = double.Parse(input);
+                        dataType = "floating point";
+                    }
+                    catch (FormatException)
+                    {
+                        try
+                        {
+                            char number = char.Parse(input);
+                            dataType = "character";
+                        }
+                        catch (FormatException)
+                        {
+                            try
+                            {
+                                bool number = bool.Parse(input);
+                                dataType = "boolean";
+                            }
+                            catch (FormatException)
+                            {
+                                dataType = "string";
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"{input} is {dataType} type");
             }
+
+            //string input = Console.ReadLine();
+            //int valueInt;
+            //float valueFloat;
+            //char valueChar;
+            //bool valueBoolean;
+
+            //while (input != "END")
+            //{
+            //    if (int.TryParse(input, out valueInt))
+            //    {
+            //        Console.WriteLine($"{input} is integer type");
+            //    }
+            //    else if (float.TryParse(input, out valueFloat))
+            //    {
+            //        Console.WriteLine($"{input} is floating point type");
+            //    }
+            //    else if (char.TryParse(input, out valueChar))
+            //    {
+            //        Console.WriteLine($"{input} is character type");
+            //    }
+            //    else if (bool.TryParse(input, out valueBoolean))
+            //    {
+            //        Console.WriteLine($"{input} is boolean type");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{input} is string type");
+            //    }
+            //    input = Console.ReadLine();
+            //}
+
         }
     }
 }
