1101a06abe2d084e5957d9e8d45399a5110335aa
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-03 22:22:38 +0300

    Add files via upload

---
+++ 03. Floating Equality/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            decimal EPS =  0.000001m;
+
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+
+            if ((Math.Abs(a - b)) < EPS)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
