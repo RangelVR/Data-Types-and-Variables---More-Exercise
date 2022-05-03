85d7a19bb8b167c1aaf07b087c562564cdebb55d
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-03 23:10:02 +0300

    Add files via upload

---
+++ 04. Refactoring: Prime Checker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopNum = int.Parse(Console.ReadLine());
+
+            for (int currNum = 2; currNum <= stopNum; currNum++)
+            {
+                string prime = "true";
+                for (int startNum = 2; startNum < currNum; startNum++)
+                {
+                    if (currNum % startNum == 0)
+                    {
+                        prime = "false";
+                        break;
+                    }
+                }
+                Console.WriteLine($"{currNum} -> {prime}");
+            }
+
+        }
+    }
+}
