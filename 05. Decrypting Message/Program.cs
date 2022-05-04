37b5c037fbb4c446c26d306b248d63e3670c5de7
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-04 21:24:56 +0300

    Add files via upload

---
+++ 05. Decrypting Message/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int nChars = int.Parse(Console.ReadLine());
+            string decriptedMessage = string.Empty;
+
+            for (int i = 0; i < nChars; i++)
+            {
+                char ch = char.Parse(Console.ReadLine());
+                char decryptedChar = (char)(ch + key);
+                decriptedMessage += decryptedChar;
+            }
+
+            Console.WriteLine(decriptedMessage);
+        }
+    }
+}
