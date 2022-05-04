702d77649e598530ca5105acd79e9f48f3deda3c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-05-04 22:45:27 +0300

    Add files via upload

---
+++ 06. Balanced Brackets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLines = int.Parse(Console.ReadLine());
+            
+            int openCounter = 0;
+            int closeCounter = 0;
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    openCounter++;
+                }
+                else if (input == ")")
+                {
+                    closeCounter++;
+                    if (openCounter - closeCounter != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+
+            }
+
+            if (openCounter == closeCounter)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+
+
+            
+        }
+    }
+}
