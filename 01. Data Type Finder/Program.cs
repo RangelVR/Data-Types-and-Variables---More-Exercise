4ba68b8e8b31b7927aea292784068eebe5d6793a
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-25 23:30:03 +0200

    Update Program.cs

--- 01. Data Type Finder/Program.cs
+++ 01. Data Type Finder/Program.cs
@@ -1,88 +1,73 @@
-using System;
+string input = Console.ReadLine();
 
-namespace _01._Data_Type_Finder
+while (input != "END")
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            string input = Console.ReadLine();
-
-            string dataType = string.Empty;
-
-            while (input != "END")
-            {
-                try
-                {
-                    int value = int.Parse(input);
-                    dataType = "integer";
-                }
-                catch (Exception)
-                {
-                    try
-                    {
-                        float value = float.Parse(input);
-                        dataType = "floating point";
-                    }
-                    catch (Exception)
-                    {
-                        try
-                        {
-                            char value = char.Parse(input);
-                            dataType = "character";
-                        }
-                        catch (Exception)
-                        {
-                            try
-                            {
-                                bool value = bool.Parse(input);
-                                dataType = "boolean";
-                            }
-                            catch (Exception)
-                            {
-
-                                dataType = "string";
-                            }
-                        }
-                    }
-                }
-                Console.WriteLine($"{input} is {dataType} type");
-
-                input = Console.ReadLine();
-            }
-
-            //string input = Console.ReadLine();
-            //int valueInt;
-            //float valueFloat;
-            //char valueChar;
-            //bool valueBoolean;
-
-            //while (input != "END")
-            //{
-            //    if (int.TryParse(input, out valueInt))
-            //    {
-            //        Console.WriteLine($"{input} is integer type");
-            //    }
-            //    else if (float.TryParse(input, out valueFloat))
-            //    {
-            //        Console.WriteLine($"{input} is floating point type");
-            //    }
-            //    else if (char.TryParse(input, out valueChar))
-            //    {
-            //        Console.WriteLine($"{input} is character type");
-            //    }
-            //    else if (bool.TryParse(input, out valueBoolean))
-            //    {
-            //        Console.WriteLine($"{input} is boolean type");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine($"{input} is string type");
-            //    }
-            //    input = Console.ReadLine();
-            //}
-
-        }
+	if (int.TryParse(input, out int integer))
+	{
+		Console.WriteLine($"{input} is integer type");
+	}
+	else if (double.TryParse(input, out double floatingPoint))
+	{
+		Console.WriteLine($"{input} is floating point type");
+	}
+	else if (char.TryParse(input, out char character))
+	{
+		Console.WriteLine($"{input} is character type");
+	}
+	else if (bool.TryParse(input, out bool boolen))
+	{
+        Console.WriteLine($"{input} is boolean type");
     }
+    else
+	{
+        Console.WriteLine($"{input} is string type");
+    }
+
+    input = Console.ReadLine();
 }
+
+--------------------------------------------------------------------------
+//string input = Console.ReadLine();
+
+//string dataType = string.Empty;
+
+//while (input != "END")
+//{
+//    try
+//    {
+//        int value = int.Parse(input);
+//        dataType = "integer";
+//    }
+//    catch (Exception)
+//    {
+//        try
+//        {
+//            float value = float.Parse(input);
+//            dataType = "floating point";
+//        }
+//        catch (Exception)
+//        {
+//            try
+//            {
+//                char value = char.Parse(input);
+//                dataType = "character";
+//            }
+//            catch (Exception)
+//            {
+//                try
+//                {
+//                    bool value = bool.Parse(input);
+//                    dataType = "boolean";
+//                }
+//                catch (Exception)
+//                {
+
+//                    dataType = "string";
+//                }
+//            }
+//        }
+//    }
+//    Console.WriteLine($"{input} is {dataType} type");
+
+//    input = Console.ReadLine();
+//}
